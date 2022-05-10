521d1a37da41f4f7fd52a2b6432f326122ede77d
Chiara <chiaratagani@hotmail.it>
2022-05-10 20:46:57 +0200

    Progetto

---
+++ Tombola/Tombola/TombolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TombolaApp.swift
+//  Tombola
+//
+//  Created by Chiara Tagani on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TombolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
